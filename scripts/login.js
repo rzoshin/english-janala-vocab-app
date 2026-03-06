2834ef012e566d8178bae4444262c2fd3cf04ab6
rzoshin <raiyan435jgcc@gmail.com>
2026-03-06 17:54:18 +0600

    Implemented whole functionality

---
+++ scripts/login.js
@@ -0,0 +1,26 @@
+console.log("Login JS is working");
+
+const btn_element = document.getElementById("login-btn");
+
+btn_element.addEventListener('click', function () {
+    // 1. Get the number
+    const inputNumber = document.getElementById("input-number");
+    const contact = inputNumber.value;
+    console.log(contact);
+    // 2. Get the Pin
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+    // 3. Match the mobile number and pin
+    if ((contact === "01713078749") && (pin === "4351")) {
+        // 3.1 true:: Homepage
+        alert("Login Successful");
+        window.location.assign("./home.html")
+    } else {
+        // 3.2 false:: alert->return
+        alert("Login Failed!");
+        return;
+    }
+
+
+})
